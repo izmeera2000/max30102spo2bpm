4d6547f2b94bf57b982df71700ffb9d9abac2d8f
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2022-05-28 01:24:33 +0800

    Update gps2.ino

--- gps2.ino
+++ gps2.ino
@@ -1,47 +1,150 @@
-#include <Arduino.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
- 
+#include <ESP8266WiFi.h>
+#include "ThingSpeak.h"
+
 TinyGPSPlus gps;
-SoftwareSerial gpsSerial(D1, D2); // RX, TX
-char buffer[100];
- 
-void printData() 
-{
-    if (gps.location.isUpdated()) {
-        double lat = gps.location.lat();
-        double lng = gps.location.lng();
- 
-        double altitude = gps.altitude.meters();
- 
-        int year = gps.date.year();
-        int month = gps.date.month();
-        int day = gps.date.day();
- 
-        int hour = gps.time.hour();
-        int minute = gps.time.minute();
-        int second = gps.time.second();
- 
-        snprintf(buffer, sizeof(buffer),
-                 "Latitude: %.8f, Longitude: %.8f, Altitude: %.2f m, "
-                 "Date/Time: %d-%02d-%02d %02d:%02d:%02d",
-                 lat, lng, altitude, year, month, day, hour, minute, second);
- 
-        Serial.println(buffer);
-    }
+SoftwareSerial SerialGPS(2, 0); 
+
+const char* ssid = "OPPORTUNITY";
+const char* password = "12345679";
+//----------- Channel details ----------------//
+unsigned long Channel_ID = 1704864; // Your Channel ID
+const char * myWriteAPIKey = "3VJIL0OO5404LO1Z"; //Your write API key
+//-------------------------------------------//
+float Latitude , Longitude;
+int year , month , date, hour , minute , second;
+String DateString , TimeString , LatitudeString , LongitudeString;
+
+WiFiClient  client;
+
+//WiFiServer server(80);
+void setup()
+{  
+//  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+  Serial.begin(115200);
+  SerialGPS.begin(9600);
+  Serial.println();
+  Serial.print("Connecting");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+//  server.begin();
+  Serial.println("Server started");
+//  Serial.println(WiFi.localIP());
 }
- 
-void setup() 
+
+void loop()
 {
-    Serial.begin(9600);
-    gpsSerial.begin(9600);
-}
- 
-void loop() 
-{
-    while (gpsSerial.available() > 0) {
-        if (gps.encode(gpsSerial.read())) {
-            printData();
-        }
+
+
+  while (SerialGPS.available() > 0)
+    if (gps.encode(SerialGPS.read()))
+          Serial.println("Searching");
+
+    {
+      if (gps.location.isValid())
+      {
+        Latitude = gps.location.lat();
+        LatitudeString = String(Latitude , 8);
+        Longitude = gps.location.lng();
+        LongitudeString = String(Longitude , 8);
+      }
+
+//      if (gps.date.isValid())
+//      {
+//        DateString = "";
+//        date = gps.date.day();
+//        month = gps.date.month();
+//        year = gps.date.year();
+//
+//        if (date < 10)
+//        DateString = '0';
+//        DateString += String(date);
+//
+//        DateString += " / ";
+//
+//        if (month < 10)
+//        DateString += '0';
+//        DateString += String(month);
+//        DateString += " / ";
+//
+//        if (year < 10)
+//        DateString += '0';
+//        DateString += String(year);
+//      }
+//
+//      if (gps.time.isValid())
+//      {
+//        TimeString = "";
+//        hour = gps.time.hour(); //adjust UTC
+//        minute = gps.time.minute();
+//        second = gps.time.second();
+//    
+//        if (hour < 10)
+//        TimeString = '0';
+//        TimeString += String(hour);
+//        TimeString += " : ";
+//
+//        if (minute < 10)
+//        TimeString += '0';
+//        TimeString += String(minute);
+//        TimeString += " : ";
+//
+//        if (second < 10)
+//        TimeString += '0';
+//        TimeString += String(second);
+//      }
+
     }
+//  WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }
+  ThingSpeak.writeField(Channel_ID, 3, LatitudeString, myWriteAPIKey);
+  ThingSpeak.writeField(Channel_ID, 4, LongitudeString, myWriteAPIKey);
+    Serial.println("Sent");
+//
+//  //Response
+//  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n <!DOCTYPE html> <html> <head> <title>NEO-6M GPS Readings</title> <style>";
+//  s += "table, th, td {border: 1px solid blue;} </style> </head> <body> <h1  style=";
+//  s += "font-size:300%;";
+//  s += " ALIGN=CENTER>NEO-6M GPS Readings</h1>";
+//  s += "<p ALIGN=CENTER style=""font-size:150%;""";
+//  s += "> <b>Location Details</b></p> <table ALIGN=CENTER style=";
+//  s += "width:50%";
+//  s += "> <tr> <th>Latitude</th>";
+//  s += "<td ALIGN=CENTER >";
+//  s += LatitudeString;
+//  s += "</td> </tr> <tr> <th>Longitude</th> <td ALIGN=CENTER >";
+//  s += LongitudeString;
+//  s += "</td> </tr> <tr>  <th>Date</th> <td ALIGN=CENTER >";
+//  s += DateString;
+//  s += "</td></tr> <tr> <th>Time</th> <td ALIGN=CENTER >";
+//  s += TimeString;
+//  s += "</td>  </tr> </table> ";
+// 
+  
+//  if (gps.location.isValid())
+//  {
+//    s += "<p align=center><a style=""color:RED;font-size:125%;"" href=""http://maps.google.com/maps?&z=15&mrt=yp&t=k&q=";
+//    s += LatitudeString;
+//    s += "+";
+//    s += LongitudeString;
+//    s += """ target=""_top"">Click here</a> to open the location in Google Maps.</p>";
+//  }
+//
+//  s += "</body> </html> \n";
+
+//  client.print(s);
+  delay(100);
+
 }
